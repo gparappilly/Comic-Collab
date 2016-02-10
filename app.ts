c5ae60c674e1ef56a7f345e0952c40378847f407
leylayazdan <yazdansh@ualberta.ca>
2016-02-10 10:11:27 -0800

    erg

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 ///<reference path='types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path='types/DefinitelyTyped/express/express.d.ts'/>
 //import session = BrowserStorage.session;
-//'use strict';
 
 interface Error {
     status?: number;
