3326f66db7eaa83b5e45f96c1bcf1b4d0a6f7c7f
Wendy Wang <wangwenusa11@gmail.com>
2016-03-07 09:49:02 -0800

    Added more resetpassword

--- test/testResetPassword.js
+++ test/testResetPassword.js
@@ -4,9 +4,27 @@ var should = require('should');
 var send = require('send');
 
 describe("Check Reset Password step 1", function() {
-    var login_url = "http://localhost:3000/resetpassword/1/";
-    request(login_url, function (error, response, body) {
+    var reset_url = "http://localhost:3000/resetpassword/1/";
+    request(reset_url, function () {
         expect(response.statusCode).to.equal(200);
     });
-
 });
+
+describe("Check Reset Password step 2", function() {
+    var username = "testuser";
+    var reset2_url = " http://localhost:3000/resetpassword/2/" + username;
+    request(reset2_url, function () {
+        expect(response.statusCode).to.equal(200);
+    });
+});
+
+describe("Check Reset Password step 3", function() {
+    var username = "testuser";
+    var reset3_url = "http://localhost:3000/resetpassword/3/" + username;
+    request(reset3_url, function () {
+        expect(response.statusCode).to.equal(200);
+    });
+});
+
+
+
